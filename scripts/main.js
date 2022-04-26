9ca856106bbf7d77bb3b1f3a731b7d7caacf54bf
chris collins <cmco339@g.uky.edu>
2022-04-26 14:50:17 -0400

    played with colors

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,18 @@
 
 // Start dark mode if it's after 6:59:59pm
-// Start dark mode if it's after 6:59:59pm
-if (new Date().getHours() > 10) {
-  //alert(new Date().getHours());
+if (new Date().getHours() > 18) {
+
   const html = document.querySelector('html');
   html.style.backgroundColor = 'black';
   html.style.color = 'white';
 }
 
-if (new Date().getHours() > 10) {
+if (new Date().getHours() > 18) {
   const header = document.querySelector('header');
   header.style.backgroundColor = 'black';
   header.style.color = 'white';
 }
-if (new Date().getHours() > 10) {
+if (new Date().getHours() > 18) {
   const footer = document.querySelector('footer');
   footer.style.backgroundColor = 'black';
   footer.style.color = 'white';
